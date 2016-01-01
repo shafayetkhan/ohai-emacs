2c24599b06168f9f308f02c2ad8297d1f7f93d6d
Shafayet Khan <shafi@layer3tv.com>
2016-01-01 00:09:48 -0500

    Add copy commit message and magit-commit-mode

--- modules/ohai-git.el
+++ modules/ohai-git.el
@@ -40,6 +40,12 @@
 (package-require 'git-messenger)
 (global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
 
+;; Copy commit message
+(define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
+
+;; Enable magit-commit-mode after typing 's', 'S', 'd'
+(add-hook 'git-messenger:popup-buffer-hook 'magit-commit-mode)
+
 ;; Convenient binding for vc-git-grep
 (global-set-key (kbd "C-x v f") 'vc-git-grep)
 
