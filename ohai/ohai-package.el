0f3956feb3909d4442517413a06aa4c03469ddcf
Shafayet Khan <shafi@layer3tv.com>
2016-01-01 00:38:08 -0500

    Add site-lisp to load-path
    Adapted from Purcell's emacs.d. site-lisp is mainly used to load
    personal projects into emacs.

--- ohai/ohai-package.el
+++ ohai/ohai-package.el
@@ -71,4 +71,21 @@
 ;; Set up $PATH
 (require 'ohai-init-exec-path)
 
+;; Set load path
+(eval-when-compile (require 'cl))
+(defun sanityinc/add-subdirs-to-load-path (parent-dir)
+  "Adds every non-hidden subdir of PARENT-DIR to `load-path'."
+  (let* ((default-directory parent-dir))
+    (progn
+      (setq load-path
+            (append
+             (remove-if-not
+              (lambda (dir) (file-directory-p dir))
+              (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
+             load-path)))))
+
+;; Add ~/.emacs.d/site-lisp/* to load path
+(sanityinc/add-subdirs-to-load-path
+ (expand-file-name "site-lisp/" user-emacs-directory))
+
 (provide 'ohai-package)
