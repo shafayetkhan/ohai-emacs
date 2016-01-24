1c5f2fb759d28c9216812d021d76747ee1159ef4
Shafayet Khan <shafi@layer3tv.com>
2016-01-24 02:55:05 -0500

    Add general config for emacs-lite

--- init-lite.el
+++ init-lite.el
@@ -63,4 +63,24 @@
   (set (make-local-variable 'truncate-lines) nil))
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
 
+;; Always ask for y/n keypress instead of typing out 'yes' or 'no'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Emacs writes backup files to `filename~` by default. This is messy,
+;; so let's tell it to write them to `~/.emacs.d/bak` instead.
+;; If you have an accident, check this directory - you might get lucky.
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name (concat dotfiles-dir "bak")))))
+
+;; Automatically save buffers before launching M-x compile and friends,
+;; instead of asking you if you want to save.
+(setq compilation-ask-about-save nil)
+
+;; Make the selection work like most people expect.
+(delete-selection-mode t)
+(transient-mark-mode t)
+
+;; Automatically update unmodified buffers whose files have changed.
+(global-auto-revert-mode 1)
+
 (load-theme 'wombat)
