7e5bfabea5475f7a89dbaba49da8ff2d8438359d
Shafayet Khan <shafi@layer3tv.com>
2016-01-23 22:40:05 -0500

    Add init-light for quick initialization
    dotfiles repo has corresponding dependencies for this to work.

---
+++ init-light.el
@@ -0,0 +1,46 @@
+;;; -*- lexical-binding: t -*-
+;;; init.el --- This is where all emacs start.
+
+;; Copyright (C) 2016 Shafayet Khan
+
+;; Author: Shafayet Khan
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;; Figure out the path to our .emacs.d by getting the path part of the
+;; current file (`init.el`).
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) (file-chase-links load-file-name))))
+
+
+(add-to-list 'load-path (concat dotfiles-dir "ohai"))
+(add-to-list 'load-path (concat dotfiles-dir "modules"))
+
+;; Define where we want to keep `loaddefs.el` (our autoload declarations) and
+;; `custom.el` (our user settings file).
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+;; Load the user settings from `custom.el`.
+(load custom-file 'noerror)
+
+(require 'ohai-personal-taste)
+
+
+;; Get rid of the training wheels, if you're ready for it.
+(when (not ohai-personal-taste/training-wheels)
+  (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+    (when (fboundp mode) (funcall mode -1))))
